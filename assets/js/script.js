f2bfed1cc27c2aebc1f98d07afedd31a1094deaa
leenabaxioned <leenab@axioned.com>
2024-05-29 17:33:40 +0530

    structure  added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 /* Author: */
+var accordionBtns = document.querySelectorAll(".slides");
+var imgText = document.querySelector(".img-text");
+
+accordionBtns.forEach((accordion) => {
+    accordion.onclick = function () {
+      accordionBtns.forEach((btn) => {
+        if (btn === this) {
+          this.classList.toggle("is-open");
+          btn.style.width = "100%";
+          //imgText.style.display = "block"
+        } else {
+          btn.style.width = "10%";
+         // imgText.style.display = "none"
+        } 
+      });
+    }
+  });
+
+ //   let content = this;
+    //   if (content!==this) {
+    //     //this is if the accordion is open
+    //     content.style.width = "45%";
+    //   } else {
+    //     //if the accordion is currently closed
+    //     content.style.width = "100%";
+    //   }
+    // };
